6fec4c9fd2e1cf011e9ffc9c796bbfb53aef8250
Dweina1920 <dina.yamanid@gmail.com>
2022-09-29 14:11:26 +0200

    actualizar-deploy

--- src/components/Elements/HeaderElements/FruitsMenu/FruitsMenu.jsx
+++ src/components/Elements/HeaderElements/FruitsMenu/FruitsMenu.jsx
@@ -6,7 +6,7 @@ function FruitsMenu(){
         <div className="fruit-menu">
                         
         <Link to='/' id="MENU-FRUIT" >HOME</Link>
-        <Link to='/sobre-nosotros' id="MENU-FRUIT" >SOBRE NOSOTRS</Link>
+        <Link to='/sobre-nosotros' id="MENU-FRUIT" >SOBRE NOSOTROS</Link>
         <Link to='/registrate' id="MENU-FRUIT" >REGÍSTRATE</Link>
         <Link to='/log-in' id="MENU-FRUIT" >LOG IN</Link>
         <Link to='/productos' id="MENU-FRUIT" >PRODUCTOS</Link>
